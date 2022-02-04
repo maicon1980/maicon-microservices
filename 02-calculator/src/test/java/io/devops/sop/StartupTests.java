1d73e399bd1d220cd0884af463876bc41a40d973
Maicon Novaes <maionnovaes@gmail.com>
2022-02-04 15:21:24 -0300

    Primeiro Commit

---
+++ 02-calculator/src/test/java/io/devops/sop/StartupTests.java
@@ -0,0 +1,13 @@
+package io.devops.sop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
